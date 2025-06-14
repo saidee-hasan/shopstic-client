0ca184cbcc8cc41eeecdcbbf1abd7249c55395d2
saidee-hasan <mdsaideehasan@gmail.com>
2025-06-14 16:35:35 +0600

    loddind dpner

--- src/components/Common/LoadingSpinner.jsx
+++ src/components/Common/LoadingSpinner.jsx
@@ -2,7 +2,7 @@ import { ImSpinner9 } from "react-icons/im";
 
 const LoadingSpinner = () => {
   return (
-    <div className="flex flex-col gap-5 justify-center items-center h-80 text-white bg-widget">
+    <div className="flex flex-col gap-5 justify-center items-center h-80 text-white bg-white">
       <ImSpinner9 className="text-6xl animate-spin" />
       <span className="font-medium">Loading...</span>
     </div>
--- src/layout/Dashboard/DashboardLayout.jsx
+++ src/layout/Dashboard/DashboardLayout.jsx
@@ -3,24 +3,24 @@ import LeftSide from "../../components/Dashboard/Layout/LeftSide";
 import Header from "../../components/Dashboard/Header/Header";
 import RightSide from "../../components/Dashboard/Layout/RightSide";
 
-
 const DashboardLayout = () => {
   return (
-    <div className="overflow-hidden"
-    style={{
-      backgroundColor:"#171f12"
-    }}
-    >
-      <div className={`flex`}>
-        <div className="hidden bg-white lg:block">
+    <div className="overflow-hidden bg-white">
+      <div className="flex">
+        {/* Left Sidebar - visible on large screens */}
+        <div className="hidden lg:block">
           <LeftSide />
         </div>
+
+        {/* Main Content Area */}
         <div className="w-full h-[calc(100vh)] overflow-y-auto bar-hidden">
           <Header />
           <div>
             <Outlet />
           </div>
         </div>
+
+        {/* Right Sidebar - visible on large screens */}
         <div className="hidden lg:block">
           <RightSide />
         </div>
@@ -30,3 +30,4 @@ const DashboardLayout = () => {
 };
 
 export default DashboardLayout;
+
